2df55dcb6e12e4d1b13967ad7dc4fb5d2ab1b40e
Michael Truell <Luca@Lucas-MacBook-Pro.local>
2015-07-14 15:54:45 -0400

    asdfasdfasdfasdf

--- BruteForceRobot/RunRAT.java
+++ BruteForceRobot/RunRAT.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import java.awt.event.InputEvent;
 import java.awt.AWTException;
 import java.net.*;
+import java.io.*;
 
 /**
  * Write a description of class RunRAT here.
@@ -16,8 +17,47 @@ import java.net.*;
  */
 public class RunRAT
 {
-    public static void main(String[] args) throws InterruptedException {
-        print("hello");
+    public static void main(String[] args) throws IOException {
+         
+        if (args.length != 2) {
+            System.err.println(
+                "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+ 
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[4445]);
+ 
+        try (
+            Socket kkSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+ 
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+                 
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        }
     }
     
     public static void print(String string) {
