f81c4b25b3d0ba06627708078b8abd7bcf34fb4c
Michael Truell <Luca@Lucas-MacBook-Pro.local>
2015-07-14 14:56:25 -0400

    was was added 420

---
+++ BruteForceRobot/RunRAT.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class RunRAT here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RunRAT
+{
+    public static void main(String[] args) throws InterruptedException {
+        print("hello");
+    }
+    
+    public static void print(String string) {
+        System.out.println(string);
+    }
+    
+    public static void openSockets(String ip) {
+        
+    }
+}
