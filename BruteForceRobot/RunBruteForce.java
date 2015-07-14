87371cb6475d720aa2c3f04b580a1aae111927ef
Michael <truell20@gmail.com>
2015-07-14 14:36:16 -0400

    adsf

--- BruteForceRobot/RunBruteForce.java
+++ BruteForceRobot/RunBruteForce.java
@@ -174,6 +174,6 @@ public class RunBruteForce {
 	}
 	
 	static void typePassword() {
-		char[] chars = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+		//char[] chars = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 	}
 }
