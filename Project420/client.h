49bfd4156491245cea98c843412935ea945536b8
Luca Koval <lucakoval@me.com>
2016-01-03 11:16:43 -0500

    I'm doing it in c++ now
    yee

---
+++ Project420/client.h
@@ -0,0 +1,14 @@
+//
+//  client.h
+//  
+//
+//  Created by Luca on 1/3/16.
+//
+//
+
+#ifndef ____client__
+#define ____client__
+
+#include <stdio.h>
+
+#endif /* defined(____client__) */
---
+++ Project420/server.h
@@ -0,0 +1,14 @@
+//
+//  server.h
+//  
+//
+//  Created by Luca on 1/3/16.
+//
+//
+
+#ifndef ____server__
+#define ____server__
+
+#include <stdio.h>
+
+#endif /* defined(____server__) */
