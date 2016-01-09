86fff6a1fc5105f9c0dc04275d438a8b1e6b9571
Luca Koval <lucakoval@me.com>
2016-01-09 12:37:58 -0500

    wut
    wut

--- Project420/C++/server.cpp
+++ Project420/C++/server.cpp
@@ -3,6 +3,7 @@
  This version runs forever, forking off a separate
  process for each connection
  */
+#include <unistd.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -21,24 +22,19 @@ const int SCREEN_HEIGHT = 480;
 void dostuff(int); /* function prototype */
 void sdlWindow();
 
-//extern "C" {
+
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-//}
 
-//extern "C" {
 int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, portno, pid;
     socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
     int count = 0;
-    //int m;
-    //char buffer2[256];
-    //char str1[] = "exit";
     
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
@@ -57,123 +53,42 @@ int main(int argc, char *argv[])
         error("ERROR on binding");
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
-    ///*
     newsockfd = accept(sockfd,
                        (struct sockaddr *) &cli_addr, &clilen);
     if (newsockfd < 0)
         error("ERROR on accept");
-    //*/
     while (1) {
-        /*
-        if (count == 0) {
-            newsockfd = accept(sockfd,
-                               (struct sockaddr *) &cli_addr, &clilen);
-            if (newsockfd < 0)
-                error("ERROR on accept");
-        }
-        */
         pid = fork();
         if (pid < 0)
             error("ERROR on fork");
         if (pid == 0)  {
             close(sockfd);
-            //dostuff(newsockfd);
             
             int n;
             char buffer[256];
             char str1[] = "exit";
-            //std::string actualString = "exitThis";
             
             bzero(buffer,256);
             n = read(newsockfd,buffer,255);
             if (n < 0) error("ERROR reading from socket");
+            
             printf("Here is the message: %s\n",buffer);
-            //wat
-            //extern "C++" {
-            /*
-             extern "C++" std::string bufferString(buffer);
-             
-             extern "C++" if (bufferString == "exitThis") {
-             //extern "C" {
-             extern "C" printf("exited");
-             extern "C" exit(0);
-             //}
-             }
-             //}
-             */
             
             if (strcmp(buffer,"exit\n") == 0) {
                 printf("The program has exited.\n");
                 exit(0);
-                //break;
             }
             system(buffer);
             n = write(newsockfd,"I got your message",18);
             if (n < 0) error("ERROR writing to socket");
-            
-            /*
-            bzero(buffer2,256);
-            m = read(newsockfd,buffer2,255);
-            if (m < 0) error("ERROR reading from socket");
-            if (strcmp(buffer2,str1) == 0) {
-                //exit(0);
-                break;
-            }
-            */
-            //exit(0);
         }
-        //else close(newsockfd);
-        //printf("cmon");
-        //count++;
-        //printf("count: %d\n",count);
     } /* end of while */
     close(sockfd);
     return 0; /* we never get here */
 }
 //}
 
-/******** DOSTUFF() *********************
- There is a separate instance of this function
- for each connection.  It handles all communication
- once a connnection has been established.
- *****************************************/
-//extern "C" {
-void dostuff (int sock)
-{
-    int n;
-    char buffer[256];
-    char str1[] = "exit";
-    //std::string actualString = "exitThis";
-    
-    bzero(buffer,256);
-    n = read(sock,buffer,255);
-    if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    //wat
-    //extern "C++" {
-    /*
-    extern "C++" std::string bufferString(buffer);
-    
-    extern "C++" if (bufferString == "exitThis") {
-        //extern "C" {
-        extern "C" printf("exited");
-        extern "C" exit(0);
-        //}
-    }
-    //}
-     */
-        
-    if (strcmp(buffer,"exit\n") == 0) {
-        printf("The program has exited.\n");
-        exit(0);
-        //break;
-    }
-    system(buffer);
-    n = write(sock,"I got your message",18);
-    if (n < 0) error("ERROR writing to socket");
-}
-//}
-
+///*
 extern "C++"{
     void sdlWindow() {
         //The window we'll be rendering to
@@ -208,7 +123,7 @@ extern "C++"{
         SDL_Quit();
     }
 }
-
+//*/
 
 
 
